6597db0c74e662914fb884263d3357086ca58cba
Ameen <ameenahmed1998@gmail.com>
2022-03-08 13:05:48 +0000

    Added changes for css

--- src/components/Output.js
+++ src/components/Output.js
@@ -21,12 +21,16 @@ class Output extends Component {
         const keys = Object.keys(experienceData)
         keys.forEach(key => {
             experienceChildrenArray.push(
-                <div key={key}>
-                    <h2>{`Position: ${experienceData[key].position || ''} `}</h2>
-                    <h2>{`Company: ${experienceData[key].company || ''} `}</h2>
-                    <h2>{`City: ${experienceData[key].city || ''} `}</h2>
-                    <h2>{`To: ${experienceData[key].to || ''} `}</h2>
-                    <h2>{`From: ${experienceData[key].from || ''} `}</h2>
+                <div key={key} className='item'>
+                    <div className='main'>
+                        <h2>{`Position: ${experienceData[key].position || ''} `}</h2>
+                        <p>{`Company: ${experienceData[key].company || ''} `}</p>
+                        <p>{`City: ${experienceData[key].city || ''} `}</p>
+                    </div>
+                    <div className='side'>
+                        <p>{`To: ${experienceData[key].to || ''} `}</p>
+                        <p>{`From: ${experienceData[key].from || ''} `}</p>
+                    </div>
                 </div>
             )
         })
@@ -41,12 +45,16 @@ class Output extends Component {
         console.log(educationData)
         keys.forEach(key => {
             educationChildrenArray.push(
-                <div key={key}>
-                    <h2>{`University: ${educationData[key].universityName}`}</h2>
-                    <h2>{`City: ${educationData[key].city}`}</h2>
-                    <h2>{`Degree Subject: ${educationData[key].degreeSubject}`}</h2>
-                    <h2>{`From: ${educationData[key].from}`}</h2>
-                    <h2>{`To: ${educationData[key].to}`}</h2>
+                <div key={key} className='item'>
+                    <div className='main'>
+                        <h2>{`University: ${educationData[key].universityName}`}</h2>
+                        <p>{`City: ${educationData[key].city}`}</p>
+                        <p>{`Degree Subject: ${educationData[key].degreeSubject}`}</p>
+                    </div>
+                    <div className='side'>
+                        <p>{`From: ${educationData[key].from}`}</p>
+                        <p>{`From: ${educationData[key].from}`}</p>
+                    </div>
                 </div>
             )
         })
@@ -69,16 +77,18 @@ class Output extends Component {
             <div className='output'>
                 <header className='header'>
                     <h1 className='fullName'>{headerInfo.name}</h1>
-                    <h2 className='address'>{headerInfo.address}</h2>
-                    <h3 className='phoneNumber'>{headerInfo.phoneNumber}</h3>
-                    <h3 className='email'>{headerInfo.email}</h3>
+                    <div class='contact-info'>
+                        <h2 className='address'>Address: {headerInfo.address}</h2>
+                        <h3 className='phoneNumber'>Phone Number: {headerInfo.phoneNumber}</h3>
+                        <h3 className='email'>Email: {headerInfo.email}</h3>
+                    </div>
                 </header>
-                <p class="description">{headerInfo.description}</p>
-                <div className="experience">
+                <p className="description">{headerInfo.description}</p>
+                <div className="section experience">
                     Experience:
                     {experienceSection}
                 </div>
-                <div className='education'>
+                <div className='section education'>
                     Education:
                     {educationSection}
                 </div>
